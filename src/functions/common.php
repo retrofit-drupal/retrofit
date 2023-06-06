731da4d6c842c76c4c360a3035f7b3a3f8e600fc
Matt Glaman <nmd.matt@gmail.com>
2023-06-05 22:35:19 -0500

    clean up phpstan

--- src/functions/common.php
+++ src/functions/common.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\RevisionableInterface;
 use Drupal\Core\Language\LanguageInterface;
 
@@ -13,7 +14,7 @@ use Drupal\Core\Language\LanguageInterface;
  *
  * @link https://git.drupalcode.org/project/drupal/-/blob/7.x/includes/common.inc#L2300
  */
-function url(?string $path = null, array $options = array()): string
+function url(?string $path = null, array $options = []): string
 {
     if ($path === null) {
         return '/';
@@ -25,35 +26,45 @@ function url(?string $path = null, array $options = array()): string
     return $path;
 }
 
-function entity_load(string $entity_type, array|false $ids = false, array $conditions = [], bool $reset = false)
+/**
+ * @param array<int, int|string>|false $ids
+ * @param array<string, mixed> $conditions
+ *
+ * @return array<int|string, EntityInterface>
+ */
+function entity_load(string $entity_type, array|false $ids = false, array $conditions = [], bool $reset = false): array
 {
     $controller = entity_get_controller($entity_type);
     if ($reset) {
         $controller->resetCache();
     }
     if ($conditions === []) {
-        return $controller->loadMultiple($ids);
+        return $controller->loadMultiple($ids ?: null);
     }
     return $controller->loadByProperties($conditions);
 }
 
-function entity_load_unchanged(string $entity_type, int|string $id)
+function entity_load_unchanged(string $entity_type, int|string $id): ?EntityInterface
 {
     return entity_get_controller($entity_type)->loadUnchanged($id);
 }
 
-function entity_get_controller(string $entity_type)
+function entity_get_controller(string $entity_type): EntityStorageInterface
 {
     // @todo should this return the storage or a decorated storage?
     return \Drupal::entityTypeManager()->getStorage($entity_type);
 }
 
-function entity_uri(string $entity_type, EntityInterface $entity)
+function entity_uri(string $entity_type, EntityInterface $entity): string
 {
+    // @phpstan-ignore-next-line
     return $entity->toUrl()->toString();
 }
 
-function entity_extract_ids(string $entity_type, EntityInterface $entity)
+/**
+ * @return array{int|string|null, int|string|null, string}
+ */
+function entity_extract_ids(string $entity_type, EntityInterface $entity): array
 {
     return [
       $entity->id(),
@@ -62,7 +73,7 @@ function entity_extract_ids(string $entity_type, EntityInterface $entity)
     ];
 }
 
-function entity_language(string $entity_type, EntityInterface $entity)
+function entity_language(string $entity_type, EntityInterface $entity): ?string
 {
     $langcode = $entity->language()->getId();
     return $langcode === LanguageInterface::LANGCODE_NOT_SPECIFIED ? null : $langcode;
