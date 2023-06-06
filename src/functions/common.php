2112abe9974e401051cf3dc75862978a8d1bf5c4
Matt Glaman <nmd.matt@gmail.com>
2023-06-05 22:48:01 -0500

    Add entity functions (#3)

--- src/functions/common.php
+++ src/functions/common.php
@@ -2,6 +2,11 @@
 
 declare(strict_types=1);
 
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\RevisionableInterface;
+use Drupal\Core\Language\LanguageInterface;
+
 /**
  * @todo flush out
  * this cannot call Url objects because they may generate routes and could
@@ -9,7 +14,7 @@ declare(strict_types=1);
  *
  * @link https://git.drupalcode.org/project/drupal/-/blob/7.x/includes/common.inc#L2300
  */
-function url(?string $path = null, array $options = array()): string
+function url(?string $path = null, array $options = []): string
 {
     if ($path === null) {
         return '/';
@@ -19,4 +24,57 @@ function url(?string $path = null, array $options = array()): string
         $path = "/$path";
     }
     return $path;
+}
+
+/**
+ * @param array<int, int|string>|false $ids
+ * @param array<string, mixed> $conditions
+ *
+ * @return array<int|string, EntityInterface>
+ */
+function entity_load(string $entity_type, array|false $ids = false, array $conditions = [], bool $reset = false): array
+{
+    $controller = entity_get_controller($entity_type);
+    if ($reset) {
+        $controller->resetCache();
+    }
+    if ($conditions === []) {
+        return $controller->loadMultiple($ids ?: null);
+    }
+    return $controller->loadByProperties($conditions);
+}
+
+function entity_load_unchanged(string $entity_type, int|string $id): ?EntityInterface
+{
+    return entity_get_controller($entity_type)->loadUnchanged($id);
+}
+
+function entity_get_controller(string $entity_type): EntityStorageInterface
+{
+    // @todo should this return the storage or a decorated storage?
+    return \Drupal::entityTypeManager()->getStorage($entity_type);
+}
+
+function entity_uri(string $entity_type, EntityInterface $entity): string
+{
+    // @phpstan-ignore-next-line
+    return $entity->toUrl()->toString();
+}
+
+/**
+ * @return array{int|string|null, int|string|null, string}
+ */
+function entity_extract_ids(string $entity_type, EntityInterface $entity): array
+{
+    return [
+      $entity->id(),
+      $entity instanceof RevisionableInterface ? $entity->getRevisionId() : null,
+      $entity->bundle()
+    ];
+}
+
+function entity_language(string $entity_type, EntityInterface $entity): ?string
+{
+    $langcode = $entity->language()->getId();
+    return $langcode === LanguageInterface::LANGCODE_NOT_SPECIFIED ? null : $langcode;
 }
--- src/functions/node.php
+++ src/functions/node.php
@@ -2,11 +2,17 @@
 
 declare(strict_types=1);
 
-use Drupal\node\Entity\Node;
-
-function node_load($nid, $vid = null, $reset = false)
+function node_load_multiple(array $nids = [], array $conditions = [], bool $reset = false)
 {
-    return Node::load($nid);
+    return entity_load('node', $nids, $conditions, $reset);
+}
+
+function node_load(int|string|null $nid = null, int|string|null $vid = null, bool $reset = false)
+{
+    $nids = (isset($nid) ? array($nid) : array());
+    $conditions = (isset($vid) ? array('vid' => $vid) : array());
+    $node = node_load_multiple($nids, $conditions, $reset);
+    return $node ? reset($node) : false;
 }
 
 function node_view($node, $view_mode = 'full', $langcode = null)
