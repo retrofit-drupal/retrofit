ae1be433c9fd15be95bc4f9e81e0cef193f21835
Matt Glaman <nmd.matt@gmail.com>
2023-07-22 08:47:24 -0500

    Add site_name and site_slogan support to variable_get

--- src/functions/variable.php
+++ src/functions/variable.php
@@ -22,5 +22,9 @@ function variable_del(string $name): void
 
 function variable_get(string $name, mixed $default = null): mixed
 {
-    return \Drupal::state()->get($name, $default);
+    return match ($name) {
+        'site_name' => \Drupal::config('system.site')->get('name') ?? $default,
+        'site_slogan' => \Drupal::config('system.site')->get('slogan') ?? $default,
+        default => \Drupal::state()->get($name, $default)
+    };
 }
