953989540573886460f56bfa88d8bbf133de1d50
Darren Oh <2293701+darrenoh@users.noreply.github.com>
2023-09-15 17:39:45 -0400

    Add module_implements() function

--- src/functions/module.php
+++ src/functions/module.php
@@ -6,3 +6,32 @@ function module_exists(string $module): bool
 {
     return \Drupal::moduleHandler()->moduleExists($module);
 }
+
+function module_implements(string $hook, ?bool $sort = false, ?bool $reset = false): array
+{
+    static $drupal_static_fast;
+    if (!isset($drupal_static_fast)) {
+        $drupal_static_fast['implementors'] = &drupal_static(__FUNCTION__, []);
+        $drupal_static_fast['sorted'] = &drupal_static(__FUNCTION__ . ':sorted', []);
+    }
+    $implementors = &$drupal_static_fast['implementors'];
+    if ($reset) {
+        $implementors  = [];
+        $sorted = [];
+        \Drupal::moduleHandler()->resetImplementations();
+    }
+    if (!isset($implementors[$hook])) {
+        $implementors[$hook] = [];
+        \Drupal::moduleHandler()->invokeAllWith($hook, function (callable $callback, string $module) use (&$implementors, $hook) {
+            $implementors[$hook][] = $module;
+        });
+    }
+    if ($sort) {
+        if (!isset($sorted[$hook])) {
+            $sorted[$hook] = $implementors[$hook];
+            sort($sorted[$hook]);
+        }
+        return $sorted[$hook];
+    }
+    return $implementors[$hook];
+}
