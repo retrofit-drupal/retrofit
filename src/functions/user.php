657c74f83bddb1a50534f5b5fefa41775fbde2ba
Darren Oh <2293701+darrenoh@users.noreply.github.com>
2023-10-20 13:03:08 -0400

    Add user_access()

--- src/functions/user.php
+++ src/functions/user.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+function user_access(string $string, ?AccountInterface $account = null): bool
+{
+    global $user;
+    if (!isset($account)) {
+        $account = $user;
+    }
+    return $account->hasPermission($string);
+}
+
 function user_is_anonymous(): bool
 {
     return \Drupal::currentUser()->isAnonymous();
