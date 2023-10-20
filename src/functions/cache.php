f0e3b7d81f5c08b5fcbac55b28b011987f3b4f26
Darren Oh <2293701+darrenoh@users.noreply.github.com>
2023-10-20 11:46:04 -0400

    Fix default cache

--- src/functions/cache.php
+++ src/functions/cache.php
@@ -13,21 +13,33 @@ function _cache_get_object($bin)
 
 function cache_get($cid, $bin = 'default')
 {
+    if ($bin == 'cache') {
+        $bin = 'default';
+    }
     return _cache_get_object($bin)->get($cid);
 }
 
 function cache_get_multiple(array &$cids, $bin = 'default')
 {
+    if ($bin == 'cache') {
+        $bin = 'default';
+    }
     return _cache_get_object($bin)->getMultiple($cids);
 }
 
 function cache_set($cid, $data, $bin = 'default', $expire = CACHE_PERMANENT)
 {
+    if ($bin == 'cache') {
+        $bin = 'default';
+    }
     return _cache_get_object($bin)->set($cid, $data, $expire);
 }
 
 function cache_clear_all($cid = null, $bin = null, $wildcard = false)
 {
+    if ($bin == 'cache') {
+        $bin = 'default';
+    }
     if (!isset($cid) && !isset($bin)) {
         try {
             _cache_get_object('render')->deleteAll();
