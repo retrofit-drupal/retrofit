2ee4970ca8da238a290ee7e142ecf68f48b0ac59
Darren Oh <2293701+darrenoh@users.noreply.github.com>
2023-11-01 13:36:33 -0400

    Temporary fix for EntityReferenceItem (#72)

--- src/Field/FieldTypePluginManager.php
+++ src/Field/FieldTypePluginManager.php
@@ -7,6 +7,7 @@ namespace Retrofit\Drupal\Field;
 use Drupal\Core\Field\FieldItemInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FieldTypePluginManager as CoreFieldTypePluginManager;
+use Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem;
 use Retrofit\Drupal\Plugin\Field\FieldType\DecoratedFieldItem;
 
 final class FieldTypePluginManager extends CoreFieldTypePluginManager
@@ -15,10 +16,15 @@ final class FieldTypePluginManager extends CoreFieldTypePluginManager
      * @param FieldItemListInterface<FieldItemInterface> $items
      * @param int $index
      * @param array<int, mixed> $values
-     * @return DecoratedFieldItem
+     * @return FieldItemInterface<string|int, mixed>
      */
-    public function createFieldItem(FieldItemListInterface $items, $index, $values = null): DecoratedFieldItem
+    public function createFieldItem(FieldItemListInterface $items, $index, $values = null): FieldItemInterface
     {
-        return new DecoratedFieldItem(parent::createFieldItem($items, $index, $values));
+        $item = parent::createFieldItem($items, $index, $values);
+        // @todo Find fix that allows all field types to be accessed as arrays without breaking typed arguments.
+        if (!($item instanceof EntityReferenceItem)) {
+            $item = new DecoratedFieldItem($item);
+        }
+        return $item;
     }
 }
