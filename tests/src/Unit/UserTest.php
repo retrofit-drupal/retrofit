5d7ecb33aab3621c021c6c4915f1220f06b6e9b7
Matt Glaman <nmd.matt@gmail.com>
2023-11-01 20:55:41 -0500

    Add user_access() (#76)
    * Add user_access()
    
    * add tests
    
    ---------
    
    Co-authored-by: Darren Oh <2293701+darrenoh@users.noreply.github.com>

--- tests/src/Unit/UserTest.php
+++ tests/src/Unit/UserTest.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Retrofit\Drupal\Tests\Unit;
 
 use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Session\AccountProxy;
 use Drupal\Core\Session\UserSession;
+use Drupal\user\RoleStorageInterface;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
@@ -32,5 +35,41 @@ final class UserTest extends TestCase
         \Drupal::setContainer($container);
 
         self::assertTrue(user_is_logged_in());
+    }
+
+    public function testUserAccess(): void
+    {
+        $container = new ContainerBuilder();
+        $accountProxy = new AccountProxy(new EventDispatcher());
+        $accountProxy->setAccount(new UserSession([
+            'uid' => 5,
+            'roles' => ['foo'],
+        ]));
+        $container->set('current_user', $accountProxy);
+
+        $entityTypeManager = $this->createMock(EntityTypeManagerInterface::class);
+        $roleStorage = $this->createMock(RoleStorageInterface::class);
+        $roleStorage->expects($this->exactly(2))
+            ->method('isPermissionInRoles')
+            ->willReturnMap([
+                ['access content', ['foo'], true],
+                ['access content', ['bar'], false],
+            ]);
+
+        $entityTypeManager
+            ->method('getStorage')
+            ->with('user_role')
+            ->willReturn($roleStorage);
+        $container->set('entity_type.manager', $entityTypeManager);
+
+        \Drupal::setContainer($container);
+
+        self::assertTrue(user_access('access content'));
+
+        $userSession = new UserSession([
+            'uid' => 6,
+            'roles' => ['bar'],
+        ]);
+        self::assertFalse(user_access('access content', $userSession));
     }
 }
