f9eb02a2abdb2aa1a92aff061c804b03e2767fbd
Matt Glaman <nmd.matt@gmail.com>
2023-11-01 22:14:14 -0500

    lint and static fixes

--- src/functions/messages.php
+++ src/functions/messages.php
@@ -7,8 +7,11 @@ use Drupal\Core\Render\Markup;
 /**
  * @return string[][]|\Drupal\Component\Render\MarkupInterface[][]
  */
-function drupal_set_message(null|string|\Stringable $message = null, string $type = 'status', bool $repeat = true):array
-{
+function drupal_set_message(
+    null|string|\Stringable $message = null,
+    string $type = 'status',
+    bool $repeat = true
+): array {
     $messenger = \Drupal::messenger();
     if ($message !== null) {
         $messenger->addMessage(Markup::create($message), $type, $repeat);
