818f6759d0a2bec5eb46a03c2482195cb63b6fb4
Darren Oh <2293701+darrenoh@users.noreply.github.com>
2023-11-10 15:33:38 -0500

    Fix links array structure

--- src/functions/theme.php
+++ src/functions/theme.php
@@ -26,6 +26,9 @@ function list_themes(bool $refresh = false): array
 function theme(string $hook, array $variables = []): MarkupInterface|string
 {
     $build['#theme'] = $hook;
+    if (function_exists('retrofit_preprocess_' . $hook)) {
+        ('retrofit_preprocess_' . $hook)($variables);
+    }
     foreach ($variables as $key => $variable) {
         $build["#$key"] = $variable;
     }
@@ -37,4 +40,39 @@ function theme(string $hook, array $variables = []): MarkupInterface|string
 function path_to_theme(): string
 {
     return \Drupal::theme()->getActiveTheme()->getPath();
+}
+
+/**
+ * @param array{
+*      links?: array{title: string, html?: bool, href?: string, attributes?: array{class?: string[]}},
+*      heading?: mixed[]
+ * } $variables
+ */
+function retrofit_preprocess_links(array &$variables): void
+{
+    if (!empty($variables['links'])) {
+        foreach ($variables['links'] as $key => &$link) {
+            $link += ['attributes' => []];
+            $link['attributes']['class'][] = $key;
+            if (isset($link['html']) && !empty($link['html'])) {
+                $link['title'] = ['#markup' => $link['title']];
+                unset($link['html']);
+            }
+            if (
+                isset($link['href'])
+                && ($url = \Drupal::pathValidator()->getUrlIfValidWithoutAccessCheck($link['href']))
+            ) {
+                $url->mergeOptions($link);
+                $link['url'] = $url;
+                unset($link['href']);
+            }
+        }
+    }
+    if (!empty($variables['heading'])) {
+        if (!empty($variables['heading']['class'])) {
+            $variables['heading']['attributes']['class'] = $variables['heading']['class'];
+            unset($variables['heading']['class']);
+        }
+        $links['#heading'] = $variables['heading'];
+    }
 }
