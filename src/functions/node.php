edbc0ce5f3d40402180dcba6a9bc050d86a5d870
Darren Oh <2293701+darrenoh@users.noreply.github.com>
2023-11-29 20:58:49 -0500

    Add node type functions

--- src/functions/node.php
+++ src/functions/node.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+use Drupal\node\Entity\NodeType;
+use Drupal\node\NodeTypeInterface;
+
 function node_load_multiple(array $nids = [], array $conditions = [], bool $reset = false)
 {
     return entity_load('node', $nids, $conditions, $reset);
@@ -20,4 +23,42 @@ function node_view($node, $view_mode = 'full', $langcode = null)
     return \Drupal::entityTypeManager()
       ->getViewBuilder('node')
       ->view($node, $view_mode, $langcode);
+}
+
+function node_type_load(string $name): ?NodeTypeInterface
+{
+    return NodeType::load($name);
+}
+
+/**
+ * @param array{
+ *   type?: string,
+ *   name?: string,
+ *   base?: string,
+ *   description?: string,
+ *   help?: string,
+ *   custom?: int,
+ *   modified?: int,
+ *   locked?: int,
+ *   disabled?: int,
+ *   is_new?: int,
+ *   has_title?: int,
+ *   title_label?: string
+ * } $info
+ */
+function node_type_set_defaults(array $info = []): NodeTypeInterface
+{
+    return NodeType::create($info);
+}
+
+function node_type_save(NodeTypeInterface $info): int
+{
+    return $info->save();
+}
+
+function node_type_delete(string $type): void
+{
+    $type = NodeType::load($type);
+    assert($type instanceof NodeTypeInterface);
+    $type->delete();
 }
