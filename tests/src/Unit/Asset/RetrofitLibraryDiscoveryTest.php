783968cbcffe0a88f318b32f5231957f45117fcb
Matt Glaman <nmd.matt@gmail.com>
2024-02-04 12:12:27 -0600

    Flush out RetrofitLibraryDiscoveryTest

--- tests/src/Unit/Asset/RetrofitLibraryDiscoveryTest.php
+++ tests/src/Unit/Asset/RetrofitLibraryDiscoveryTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Retrofit\Drupal\Tests\Unit\Asset;
 
+use Drupal\Core\Asset\LibraryDiscoveryInterface;
 use PHPUnit\Framework\TestCase;
+use Retrofit\Drupal\Asset\RetrofitLibraryDiscovery;
 
 /**
  * @coversDefaultClass \Retrofit\Drupal\Asset\RetrofitLibraryDiscovery
@@ -19,7 +21,25 @@ final class RetrofitLibraryDiscoveryTest extends TestCase
      */
     public function testGetLibrariesByExtension(): void
     {
-        $this->markTestIncomplete();
+        $inner = $this->createMock(LibraryDiscoveryInterface::class);
+        $inner->expects($this->once())
+            ->method('getLibrariesByExtension')
+            ->with('system')
+            ->willReturn(['drupal.system' => []]);
+        $sut = new RetrofitLibraryDiscovery($inner);
+        self::assertEquals([], $sut->getLibrariesByExtension('retrofit'));
+        self::assertEquals(
+            ['drupal.system' => []],
+            $sut->getLibrariesByExtension('system')
+        );
+
+        $sut->setRetrofitLibrary('foo', []);
+        self::assertEquals(
+            [
+                'foo' => ['license' => [],]
+            ],
+            $sut->getLibrariesByExtension('retrofit')
+        );
     }
 
     /**
@@ -29,7 +49,25 @@ final class RetrofitLibraryDiscoveryTest extends TestCase
      */
     public function testGetLibraryByName(): void
     {
-        $this->markTestIncomplete();
+        $inner = $this->createMock(LibraryDiscoveryInterface::class);
+        $inner->expects($this->once())
+            ->method('getLibraryByName')
+            ->with('system', 'drupal.system')
+            ->willReturn([]);
+        $sut = new RetrofitLibraryDiscovery($inner);
+        self::assertFalse($sut->getLibraryByName('retrofit', 'foo'));
+        self::assertEquals(
+            [],
+            $sut->getLibraryByName('system', 'drupal.system')
+        );
+
+        $sut->setRetrofitLibrary('foo', []);
+        self::assertEquals(
+            [
+                'license' => [],
+            ],
+            $sut->getLibraryByName('retrofit', 'foo')
+        );
     }
 
     /**
@@ -38,7 +76,37 @@ final class RetrofitLibraryDiscoveryTest extends TestCase
      */
     public function testClearCachedDefinitions(): void
     {
-        $this->markTestIncomplete();
+        $inner = $this->createMock(LibraryDiscoveryInterface::class);
+        $inner->expects($this->once())
+            ->method('clearCachedDefinitions');
+        $sut = new RetrofitLibraryDiscovery($inner);
+        $sut->clearCachedDefinitions();
+    }
+
+    /**
+     * @dataProvider retrofitLibraryData
+     */
+    public function testSetRetrofitLibrary(string $key, array $attachments, array $expected): void
+    {
+        $sut = new RetrofitLibraryDiscovery(
+            $this->createMock(LibraryDiscoveryInterface::class)
+        );
+        $sut->setRetrofitLibrary($key, $attachments);
+        self::assertEquals(
+            [$key => $expected],
+            $sut->getLibrariesByExtension('retrofit')
+        );
+    }
+
+    public static function retrofitLibraryData(): \Generator
+    {
+        yield [
+            'bar',
+            [],
+            [
+                'license' => [],
+            ]
+        ];
     }
 
 }
